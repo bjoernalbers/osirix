9846915694000c673d2c468035a7c7962719d8b3
Björn Albers <bjoernalbers@googlemail.com>
2013-10-18 17:54:56 +0200

    Initial commit
    ...after running:
    
      `berks cookbook osirix --skip-vagrant --skip-test-kitchen --foodcritic`

---
+++ README.md
@@ -0,0 +1,13 @@
+# osirix cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Björn Albers (<bjoernalbers@googlemail.com>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'osirix'
+maintainer       'Björn Albers'
+maintainer_email 'bjoernalbers@googlemail.com'
+license          'Apache 2.0'
+description      'Installs/Configures osirix'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
